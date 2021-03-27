681acf2e98fca83073509648e27c61313e535aa6
crackair <77182421+crackair@users.noreply.github.com>
2021-03-27 20:02:46 +0800

    Squashed commit of the following:
    commit a57306f99dc63f4e2a35366afffa8cd44df3366e
    Author: crackair <77182421+crackair@users.noreply.github.com>
    Date:   Sat Mar 27 19:47:46 2021 +0800
    
        Fix cert domain failed cause program exit
    
    commit fa8c8660bbb283f7d684b8cbe1e3e0dc24004d8c
    Author: crackair <77182421+crackair@users.noreply.github.com>
    Date:   Sat Mar 27 14:48:56 2021 +0800
    
        Update version to 0.4.1

--- common/legocmd/lego.go
+++ common/legocmd/lego.go
@@ -3,6 +3,7 @@
 package legocmd
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"path"
@@ -10,7 +11,7 @@ import (
 	"runtime"
 	"strings"
 
-	"github.com/go-acme/lego/v4/cmd"
+	"github.com/XrayR-project/XrayR/common/legocmd/cmd"
 	"github.com/urfave/cli"
 )
 
@@ -53,6 +54,21 @@ func New() (*LegoCMD, error) {
 
 // DNSCert cert a domain using DNS API
 func (l *LegoCMD) DNSCert(domain, email, provider string, DNSEnv map[string]string) (CertPath string, KeyPath string, err error) {
+	defer func() (string, string, error) {
+		// Handle any error
+		if r := recover(); r != nil {
+			switch x := r.(type) {
+			case string:
+				err = errors.New(x)
+			case error:
+				err = x
+			default:
+				err = errors.New("unknow panic")
+			}
+			return "", "", err
+		}
+		return CertPath, KeyPath, nil
+	}()
 	// Set Env for DNS configuration
 	for key, value := range DNSEnv {
 		os.Setenv(key, value)
@@ -77,12 +93,26 @@ func (l *LegoCMD) DNSCert(domain, email, provider string, DNSEnv map[string]stri
 
 // HTTPCert cert a domain using http methods
 func (l *LegoCMD) HTTPCert(domain, email string) (CertPath string, KeyPath string, err error) {
+	defer func() (string, string, error) {
+		// Handle any error
+		if r := recover(); r != nil {
+			switch x := r.(type) {
+			case string:
+				err = errors.New(x)
+			case error:
+				err = x
+			default:
+				err = errors.New("unknow panic")
+			}
+			return "", "", err
+		}
+		return CertPath, KeyPath, nil
+	}()
 	// First check if the certificate exists
 	CertPath, KeyPath, err = checkCertfile(domain)
 	if err == nil {
 		return CertPath, KeyPath, err
 	}
-
 	argstring := fmt.Sprintf("lego -a -d %s -m %s --http run", domain, email)
 	err = l.cmdClient.Run(strings.Split(argstring, " "))
 
@@ -99,6 +129,21 @@ func (l *LegoCMD) HTTPCert(domain, email string) (CertPath string, KeyPath strin
 //RenewCert renew a domain cert
 func (l *LegoCMD) RenewCert(domain, email, certMode, provider string, DNSEnv map[string]string) (CertPath string, KeyPath string, err error) {
 	var argstring string
+	defer func() (string, string, error) {
+		// Handle any error
+		if r := recover(); r != nil {
+			switch x := r.(type) {
+			case string:
+				err = errors.New(x)
+			case error:
+				err = x
+			default:
+				err = errors.New("unknow panic")
+			}
+			return "", "", err
+		}
+		return CertPath, KeyPath, nil
+	}()
 	if certMode == "http" {
 		argstring = fmt.Sprintf("lego -a -d %s -m %s --http renew --days 30", domain, email)
 	} else if certMode == "dns" {
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.16
 
 require (
 	github.com/StackExchange/wmi v0.0.0-20210224194228-fe8f1750fd46 // indirect
-	github.com/deckarep/golang-set v1.7.1 // indirect
+	github.com/deckarep/golang-set v1.7.1
 	github.com/fsnotify/fsnotify v1.4.9
 	github.com/go-acme/lego/v4 v4.2.0
 	github.com/go-ole/go-ole v1.2.5 // indirect
@@ -13,9 +13,11 @@ require (
 	github.com/juju/ratelimit v1.0.1
 	github.com/shirou/gopsutil v3.21.2+incompatible
 	github.com/spf13/viper v1.7.1
+	github.com/stretchr/testify v1.7.0
 	github.com/tklauser/go-sysconf v0.3.4 // indirect
 	github.com/urfave/cli v1.22.4
 	github.com/xtls/xray-core v1.4.0
+	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.25.0
 )
