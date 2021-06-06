03051d3a582235e405fd0e25d2ac942fef27aea3
crackair <77182421+crackair@users.noreply.github.com>
2021-06-06 18:37:32 +0800

    feat: add fallback support for vless and trojan

--- service/controller/config.go
+++ service/controller/config.go
@@ -1,13 +1,15 @@
 package controller
 
 type Config struct {
-	ListenIP            string      `mapstructure:"ListenIP"`
-	SendIP              string      `mapstructure:"SendIP"`
-	UpdatePeriodic      int         `mapstructure:"UpdatePeriodic"`
-	CertConfig          *CertConfig `mapstructure:"CertConfig"`
-	EnableDNS           bool        `mapstructure:"EnableDNS"`
-	DNSType             string      `mapstructure:"DNSType"`
-	EnableProxyProtocol bool        `mapstructure:"EnableProxyProtocol"`
+	ListenIP            string            `mapstructure:"ListenIP"`
+	SendIP              string            `mapstructure:"SendIP"`
+	UpdatePeriodic      int               `mapstructure:"UpdatePeriodic"`
+	CertConfig          *CertConfig       `mapstructure:"CertConfig"`
+	EnableDNS           bool              `mapstructure:"EnableDNS"`
+	DNSType             string            `mapstructure:"DNSType"`
+	EnableProxyProtocol bool              `mapstructure:"EnableProxyProtocol"`
+	EnableFallback      bool              `mapstructure:"EnableFallback"`
+	FallBackConfigs      []*FallBackConfig `mapstructure:"FallBackConfigs"`
 }
 
 type CertConfig struct {
@@ -18,4 +20,11 @@ type CertConfig struct {
 	Provider   string            `mapstructure:"Provider"` // alidns, cloudflare, gandi, godaddy....
 	Email      string            `mapstructure:"Email"`
 	DNSEnv     map[string]string `mapstructure:"DNSEnv"`
+}
+
+type FallBackConfig struct {
+	SNI              string `mapstructure:"SNI"`
+	Path             string `mapstructure:"Path"`
+	Dest             string `mapstructure:"Dest"`
+	ProxyProtocolVer int    `mapstructure:"ProxyProtocolVer"`
 }
